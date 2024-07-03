df0942e89f617787935e65564e7cd52acd89c2de
Rishikesh Sharma <wrongdirection72@gmail.com>
2024-07-03 09:48:11 +0545

    basic configuration

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
 app.listen(3000, () => {
